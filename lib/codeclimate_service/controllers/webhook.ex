9d16ff7021e449ced4d23642f814dc7375feb010
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2016-03-12 21:25:37 -0300

    Add github notifier status of PR

--- lib/codeclimate_service/controllers/webhook.ex
+++ lib/codeclimate_service/controllers/webhook.ex
@@ -2,10 +2,11 @@ defmodule CodeclimateService.Controllers.Webhook do
   use Sugar.Controller
 
   def create(conn, _args) do
-    if get_req_header(conn, "X-GitHub-Event") == ["pull_request"] do
-      conn |> status(201) |> json(%{"message" => conn.params })
+    if get_req_header(conn, "x-github-event") == ["pull_request"] do
+      CodeclimateService.Services.GithubNotifier.notify(conn.params, 'pending')
+      conn |> status(201) |> json(%{"message" => "Created new build"})
      else
-       conn |> status(200) |>json(%{"message" => "Is not a pull request, moving on"})
+       conn |> status(200) |> json(%{"message" => "Is not a pull request, moving on"})
      end
   end
 end
